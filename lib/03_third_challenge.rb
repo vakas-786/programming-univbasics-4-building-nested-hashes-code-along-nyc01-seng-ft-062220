1b0438d20cc5ab3f0b8129d98963c7f9c3a3729d
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-11 21:09:18 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,12 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
    :capulet => {

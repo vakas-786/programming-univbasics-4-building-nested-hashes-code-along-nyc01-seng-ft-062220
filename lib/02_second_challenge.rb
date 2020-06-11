749871794a737056e95b08c523eec21c6a9d627f
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-11 21:05:00 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,15 +2,15 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {},
-       :matriarch => {}, 
-       :hero => {}, 
+       :matriarch => {},
+       :hero => {},
        :hero_friends => []
      },
      :capulet => {
        :patriarch => {},
-       :matriarch => {}, 
-       :heroine => {}, 
+       :matriarch => {},
+       :heroine => {},
        :heroine_friends => []
-     }     
-} 
+     }
+}
 end

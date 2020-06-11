5b2f3d7e68c9fb1bc5c7e73790c762405a568940
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-11 21:36:36 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }

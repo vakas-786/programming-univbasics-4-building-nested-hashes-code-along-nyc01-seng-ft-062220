6f4ad2b829893c1e8b5d01eb2b8202fe4aa94620
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-11 21:28:10 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,8 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benevolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-haeded"}
-      ]
+        {name: "Mercutio", age: "18", attitude: "hot-haeded"}]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -15,8 +14,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"}
-        {name: "Nurse", age: "44", attitude: "worried"}
-      ]
+        {name: "Nurse", age: "44", attitude: "worried"}]
    }
 }
 
